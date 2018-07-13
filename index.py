3d45b6c1cbbd6d9ab2811a050876ff3a06e31e7d
Erik Partridge <erikdevelopments@gmail.com>
2018-07-13 13:11:08 -0400

    Update index.py

--- index.py
+++ index.py
@@ -12,32 +12,7 @@ writer = SummaryWriter()
 # the amount per ingredient gets encoded into a single line 
 # then each flavor 
 
-INPUT_SIZE = 101
-
 INGREDIENT_EMBEDDING_SIZE = 20
-class TasteTester(nn.Module):
-    def __init__(self, input_size, hidden_size):
-        super(TasteTester, self).__init__()
-        self.embed = nn.Embedding(input_size, INGREDIENT_EMBEDDING_SIZE)
-        self.linear = nn.Linear(INGREDIENT_EMBEDDING_SIZE, hidden_size)
-        self.do = nn.Dropout(p=0.05)
-        self.l1 = nn.Linear(3000, 1)
-
-    def forward(self, x):
-        x = self.embed(x)
-        x = self.linear(x)
-        x = F.softmax(x)
-        x = self.do(x)
-        x = x.view(x.numel())
-        x = self.l1(x)
-        x = F.relu(x)
-        return x
-      
-      
-tt = TasteTester(INPUT_SIZE, 150)
-
-criterion = nn.MSELoss()
-optimizer = optim.Adamax(tt.parameters())
 
 
 ###
@@ -79,11 +54,35 @@ ys = torch.from_numpy((np.array(targets) - 3.5) / 1.4)
 print(ys)
 print(len(INGREDIENT_DICTIONARY))
 
+##
+class TasteTester(nn.Module):
+    def __init__(self, len(INGREDIENT_DICTIONARY), hidden_size):
+        super(TasteTester, self).__init__()
+        self.embed = nn.Embedding(input_size, INGREDIENT_EMBEDDING_SIZE)
+        self.linear = nn.Linear(INGREDIENT_EMBEDDING_SIZE, hidden_size)
+        self.do = nn.Dropout(p=0.05)
+        self.l1 = nn.Linear(3000, 1)
+
+    def forward(self, x):
+        x = self.embed(x)
+        x = self.linear(x)
+        x = F.softmax(x)
+        x = self.do(x)
+        x = x.view(x.numel())
+        x = self.l1(x)
+        x = F.relu(x)
+        return x
+      
+      
+tt = TasteTester(INPUT_SIZE, 150)
+
+criterion = nn.MSELoss()
+optimizer = optim.Adamax(tt.parameters())
 ###
 
 for epoch in range(40):  # loop over the dataset multiple times
     running_loss = 0.0
-    for i in range(105):
+    for i in range(len(INGREDIENT_DICTIONARY)):
         # zero the parameter gradients
         optimizer.zero_grad()
         data = rcps[i]
